c294471cab7b91932ccaf5a5c848ca036b7c69ae
Chris Chares <chris@chares.io>
2016-10-24 21:05:33 -0600

    tests and client side tc

--- test/browser.js
+++ test/browser.js
@@ -1,11 +1,7 @@
-var chai = require('chai');
-var expect = chai.expect;
-var tc = require('../js/tinycoord.js');
-
+var tc = tinycoord;
 
 describe('Conversion', function() {
   it('should encode', function() {
-
     var encoded = tc.encode(19, {
     	latitude: 104.33324432373047,
     	longitude: -39.44114303588867
@@ -15,11 +11,8 @@ describe('Conversion', function() {
   });
 
   it('should decode', function() {
-    
     var string = "E5+q0EK7wx3C";
     var decoded = tc.decode(string);
-
     expect(decoded.zoom).to.equal(19);
   });
-
 });
